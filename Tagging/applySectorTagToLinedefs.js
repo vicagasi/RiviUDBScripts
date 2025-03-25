aef415271c19fbf4c3ca063f47dc07cf1a33f3b1
Vic G <vicagasi@ut.utm.edu>
2025-03-25 18:52:09 -0400

    Improved applySectorTagToLinedefs and finished the stepwise tagger

--- Tagging/applySectorTagToLinedefs.js
+++ Tagging/applySectorTagToLinedefs.js
@@ -4,28 +4,33 @@
 
 `#name Sector Tag to Linedef Tags`;
 
-`#description Replaces the tag of each selected sector's linedefs with the sector's tag. Does not support multiple tags.`;
+`#description Replaces the tags of each selected sector's linedefs with the sector's tags.`;
+
 
 // Get the selected sectors
 let sectors = UDB.Map.getSelectedSectors();
 
+// Check if any sectors are selected
 if(sectors.length == 0)
     UDB.die('You have to select at least one sector!');
 
 sectors.forEach(
     sec => { 
         let sdef = sec.getSidedefs();
-        let t = sec.getTags();
+        let tags = sec.getTags();
         sdef.forEach(
             side => {
-                let ldef = side.line;
+                let linedef = side.line;
 
-                // Remove the first tag
-                let victim = ldef.getTags();
-                ldef.removeTag(victim[0]);
+                // Clear lines
+                linedef.getTags().forEach(x => {
+                    linedef.removeTag(x);
+                });
 
-                // Add new tag
-                ldef.addTag(t[0]);
+                // Add new tags
+                for(let i = 0; i < tags.length; i++){
+                    linedef.addTag(tags[i]);
+                }
             }
         )
     });
--- Tagging/stepwiseIncrementTagger.js
+++ Tagging/stepwiseIncrementTagger.js
@@ -6,7 +6,66 @@
 
 `#description Increments the tag every x steps.`;
 
-let lines = UDB.Map.getSelectedOrHighlightedLinedefs();
+`#scriptoptions
 
+start_tag
+{
+	description = "Starting Tag";
+	default = 1;
+	type = 0; // Integer
+}
+
+step_value
+{
+	description = "Step Value";
+	default = 1;
+	type = 0; // Integer
+}
+
+add_tags
+{
+	description = "Additive Tagging (UDMF only)";
+	default = "false";
+	type = 3; // Boolean
+}
+
+`;
+
+/* STEP INCREMENT
+* @param {int} x - input number
+* @returns {int} The value of the input number at the current step
+*/
+function stepIncrement(x){
+    return Math.floor((x - 1) / UDB.ScriptOptions.step_value + 1)
+}
+
+// Set up variables
+let lines = UDB.Map.getSelectedOrHighlightedLinedefs();
+let startingValue = UDB.ScriptOptions.start_tag * UDB.ScriptOptions.step_value;
+let addTags = UDB.ScriptOptions.add_tags;
+
+// Check if theres lines actually selected
 if(lines.length == 0)
     UDB.die('You have to select at least one linedef!');
+
+// Check if starting tag is greater than zero
+if(startingValue <= 0)
+    UDB.die("Tag can't be 0 or less!");
+
+// Check if the format supports multiple line tags
+if(!(UDB.Map.isUDMF) && clearTags)
+    UDB.die('Format does not support multiple tags! (turn off additive tagging)');
+
+// Clear tags from the linedefs
+if(!addTags){
+    lines.forEach(e => {
+        e.getTags().forEach(x => {
+            e.removeTag(x);
+        })
+    });
+}
+
+// Add tags baby
+for(let i = 0; i < lines.length; i++){
+    lines[i].addTag(stepIncrement(i + startingValue));
+}
